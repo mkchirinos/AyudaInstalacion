fe161010a6e04b2e45562b8f0d88db3231f97a55
mkchirinos <m.chirinos.cuba@isur.edu.pe>
2013-11-17 21:06:50 -0500

    finish

---
+++ AyudaInstalacion/Instalacion.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AyudaInstalacion
+{
+    /// <summary>
+    /// Interaction logic for Instalacion.xaml
+    /// </summary>
+    public partial class Instalacion : Window
+    {
+        public Instalacion()
+        {
+            InitializeComponent();
+            
+            
+        }
+        private delegate void UpdateProgressBarDelegate(System.Windows.DependencyProperty dp, Object value);
+        private void Process()
+        {
+            
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = short.MaxValue;
+            progressBar1.Value = 0;
+
+            
+            double value = 0;
+
+           UpdateProgressBarDelegate updatePbDelegate = new UpdateProgressBarDelegate(progressBar1.SetValue);
+
+            
+            do
+            {
+                value += 6;
+
+                Dispatcher.Invoke(updatePbDelegate,
+                    System.Windows.Threading.DispatcherPriority.Background,
+                    new object[] { ProgressBar.ValueProperty, value });
+
+            }
+            while (progressBar1.Value != progressBar1.Maximum);
+            label2.Content = "The installation was successful";
+            button1.Content = "Finish";
+            button2.IsEnabled = false;
+            button3.IsEnabled = false;
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (button1.Content == "Finish")
+            {
+                this.Owner.Close();
+                this.Close();
+            }
+            else
+            {
+                progressBar1.IsEnabled = true;
+                Process();
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            
+            this.Close();
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            
+            this.Owner.Close();
+            this.Close();
+        }
+
+        
+    }
+}
---
+++ AyudaInstalacion/Terminos.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AyudaInstalacion
+{
+    /// <summary>
+    /// Interaction logic for Terminos.xaml
+    /// </summary>
+    public partial class Terminos : Window
+    {
+        public Terminos()
+        {
+            InitializeComponent();
+            
+            scrollViewer1.Content = System.IO.File.ReadAllText("TerminosServicios.txt");
+            
+            
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (radioButton1.IsChecked == true)
+            {
+                
+                Instalacion install = new Instalacion();
+                install.Owner = this;
+                install.Show();
+
+            }
+            
+            
+        }
+
+        
+    }
+}
